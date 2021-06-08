a6958c0e83dfff6b56cac29b3a2fbd0b57af102c
khelechy <onyekwerekelechimac@gmail.com>
2021-06-08 22:20:20 +0100

    created nuget package

--- obj/Debug/net5.0/CSJsonDB.AssemblyInfo.cs
+++ obj/Debug/net5.0/CSJsonDB.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CSJsonDB")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("khelechy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
